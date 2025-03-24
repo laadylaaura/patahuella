380ba26aa6d27b1c712c7b5419ad7aa502c7af60
antoniDavidGarciaPomo <antonidavidgarciapomo@gmail.com>
2025-03-24 18:09:44 +0100

    Añadido sticky header y eliminado favoritos de la sesion

--- vistas/inc/header.php
+++ vistas/inc/header.php
@@ -1,62 +1,61 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/css/header.css" rel="stylesheet">
   <title>Pata y huella</title>
 </head>
-<body>
-<?php
-require_once("./lib/GestorSesiones.php");
-$gestorSesiones = new GestorSesiones();
-$usuarioLogueado = $gestorSesiones->existeSesion("CLAVE");
-?>
-<header>
-  <div class="navbar">
-    <div class="container">
-      <a href="<?php echo $ruta; ?>" class="navbar-brand d-flex align-items-center">
-        <strong>Pata y huella</strong>
-      </a>
-      
-      <div class="d-flex">
-      <strong>
-        <ul class="navbar-nav flex-row ms-auto gap-3">
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo $ruta; ?>">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo $ruta; ?>restaurantes">Restaurantes</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo $ruta; ?>hoteles">Alojamientos</a>
-          </li>
-          
-          <?php if ($usuarioLogueado): ?>
-          <!-- Opciones para usuarios logueados -->
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo $ruta; ?>favoritos/listarFavoritos">Favoritos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo $ruta; ?>usuario/listarMiPerfil">Mi Perfil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo $ruta; ?>inicio/cerrarSesion">Cerrar sesión</a>
-          </li>
-          <?php else: ?>
-          <!-- Opciones para usuarios no logueados -->
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo $ruta; ?>inicio/sesionLogin">Iniciar sesión</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="<?php echo $ruta; ?>usuario/registro">Registrarse</a>
-          </li>
-          <?php endif; ?>
-        </ul>
-</strong>
-      </div>
-    </div>
-  </div>
-</header>
 
-<div class="content-wrapper">
+<body>
+  <?php
+  require_once("./lib/GestorSesiones.php");
+  $gestorSesiones = new GestorSesiones();
+  $usuarioLogueado = $gestorSesiones->existeSesion("CLAVE");
+  ?>
+  <header class="sticky-header">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a href="<?php echo $ruta; ?>" class="navbar-brand d-flex align-items-center">
+          <strong>Pata y huella</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo $ruta; ?>">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo $ruta; ?>restaurantes">Restaurantes</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo $ruta; ?>hoteles">Alojamientos</a>
+            </li>
+            <?php if ($usuarioLogueado): ?>
+              <!-- Opciones para usuarios logueados -->
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo $ruta; ?>usuario/listarMiPerfil">Mi Perfil</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo $ruta; ?>inicio/cerrarSesion">Cerrar sesión</a>
+              </li>
+            <?php else: ?>
+              <!-- Opciones para usuarios no logueados -->
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo $ruta; ?>inicio/sesionLogin">Iniciar sesión</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo $ruta; ?>usuario/registro">Registrarse</a>
+              </li>
+            <?php endif; ?>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <div class="content-wrapper"></div>
