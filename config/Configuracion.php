a0d4c4e5a62eec4f6d8670118a83f10d18589335
antoniDavidGarciaPomo <antonidavidgarciapomo@gmail.com>
2025-05-25 15:33:44 +0200

    Añadida logica de rutas en configuracion

--- config/Configuracion.php
+++ config/Configuracion.php
@@ -3,16 +3,42 @@ class Configuracion{
 
 private static $instance = null;
 
-private  $rutaServidor ="http://localhost/patahuella/" ;
-private  $servidorBD ="localhost";
-private  $usuarioBD ="root";
-private  $passwordBD ="";
-private  $nombreBD ="patayhuella";
+
+private  $rutaServidor  ;
+private  $servidorBD;
+private  $usuarioBD ;
+private  $passwordBD ;
+private  $nombreBD ;
 
 
 
 public function __construct(Type $var = null) {
-    $this->var = $var;
+
+    if (file_exists("preproducion.txt")){
+        echo("preproduccion");
+        $this->rutaServidor= "pruebaspatahuella.infy.uk";
+        $this->servidorBD= "sql109.infinityfree.com";
+        $this->usuarioBD="f0_38869378" ;
+        $this->passwordBD= "kc57M5kSBO";
+        $this->nombreBD= "if0_38869378_pruebaspatahuella";   
+    }
+    else if (file_exists("produccion.txt")){
+        echo("proooo");
+        $this->rutaServidor= "http://patahuella.42web.io/";
+        $this->servidorBD= "sql312.infinityfree.com";
+        $this->usuarioBD="if0_38876893" ;
+        $this->passwordBD= "nebEyY54BcbO5SS";
+        $this->nombreBD= "if0_38876893_produccionpatahuella";   
+    }
+
+    else {
+        $this->rutaServidor= "http://localhost/patahuella/";
+        $this->servidorBD= "localhost";
+        $this->usuarioBD="root" ;
+        $this->passwordBD= "";
+        $this->nombreBD= "patayhuella";   
+    }
+    
 }
 
 public function getRutaServidor(){
